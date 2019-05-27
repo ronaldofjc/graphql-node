2a4b13fd3a033d10a1289ae34d8eba8a80ccd35d
Ronaldo <rfeitosa@ciandt.com>
2019-05-27 08:43:29 -0300

    Insert enum UserStatus

--- schema-query/data/db.js
+++ schema-query/data/db.js
@@ -15,21 +15,24 @@ const users = [
     name: "Ronaldo",
     email: "ronaldofjc@hotmail.com",
     age: 35,
-    profile_id: 1
+    profile_id: 1,
+    status: "ACTIVE"
   },
   {
     id: 2,
     name: "Rebecca",
     email: "rebecca@hotmail.com",
     age: 30,
-    profile_id: 2
+    profile_id: 2,
+    status: "INACTIVE"
   },
   {
     id: 3,
     name: "Ana",
     email: "ana@hotmail.com",
     age: 40,
-    profile_id: 1
+    profile_id: 1,
+    status: "BLOCKED"
   }
 ];
 
