4049356b036b3f4e72db966d26257c0990cb591a
Ronaldo <rfeitosa@ciandt.com>
2019-05-27 20:37:59 -0300

    Validate email in resolver

--- mutation/resolvers/Mutation.js
+++ mutation/resolvers/Mutation.js
@@ -1,12 +1,16 @@
 const { users, nextId } = require("../data/db");
 
 module.exports = {
-  newUser(_, { name, email, age }) {
+  newUser(_, args) {
+    const foundEmail = users.some(u => u.email === args.email);
+
+    if (foundEmail) {
+      throw new Error("Email já cadastrado!");
+    }
+
     const user = {
       id: nextId,
-      name,
-      email,
-      age,
+      ...args,
       profile_id: 1,
       status: "ACTIVE"
     };
