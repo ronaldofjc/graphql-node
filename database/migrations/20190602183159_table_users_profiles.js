a2fa1db07a0e71a3494e1bf5aa456607937680ad
Ronaldo <rfeitosa@ciandt.com>
2019-06-04 21:29:35 -0300

    Create Migration users_profiles

--- database/migrations/20190602183159_table_users_profiles.js
+++ database/migrations/20190602183159_table_users_profiles.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("users_profiles", table => {
+    table.integer("user_id").unsigned();
+    table.integer("profile_id").unsigned();
+    table.foreign("user_id").references("users.id");
+    table.foreign("profile_id").references("profiles.id");
+    table.primary(["user_id", "profile_id"]);
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("users_profiles");
 };
